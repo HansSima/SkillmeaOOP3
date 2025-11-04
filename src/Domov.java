56444ef855f5c5ed4fa2ddba5e1273174c090059
Jan Sima <simahonza@seznam.cz>
2025-11-04 18:36:01 +0100

    Konstruktor

--- src/Domov.java
+++ src/Domov.java
@@ -1,13 +1,18 @@
 
 public class Domov {
+    //Toto jsou vlastnosti (fields)
     float rozloha;
     int pocetOken;
     String material;
     int pocetPoschodi;
     boolean jeAlarmZapnuty;
 
+    //Toto je konstruktor
+    Domov(){
+        System.out.println("Toto se zavola pri vytvoreni noveho objektu");
+    }
 
-
+    //Toto je metoda
     public float vypocetCenyDomu(int lukrativniOblast, String jmenoRodiny){
         System.out.println(jmenoRodiny);
         float vypocet = lukrativniOblast* (pocetOken*800 + rozloha*840);
